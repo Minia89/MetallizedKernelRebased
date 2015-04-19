9210b59cb2c552c70b8b2026ed59d3d0a01da744
Minia89 <n.miniati@gmail.com>
2015-04-19 14:48:14 +0200

    Revert "introduce: elementalx governor"
    This reverts commit 1f1976d61863657fd80bc614cbb4a7a972247e4f.
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	drivers/gpu/msm/kgsl_pwrctrl.c
    	include/linux/cpufreq.h

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -49,10 +49,6 @@
 /* Number of jiffies for a full thermal cycle */
 #define TH_HZ			20
 
-#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
-int graphics_boost = 6;
-#endif
-
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -232,9 +228,6 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel,
 			pwrlevel->gpu_freq);
 
-#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
-graphics_boost = pwr->active_pwrlevel;
-#endif
 }
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 
