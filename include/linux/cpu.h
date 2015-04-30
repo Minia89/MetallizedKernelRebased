9fa01251e62ccb60cf14608aa5a435ebd0b82a59
Minia89 <n.miniati@gmail.com>
2015-04-30 17:37:09 +0200

    cpu.h

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -36,6 +36,8 @@ extern void cpu_remove_dev_attr(struct device_attribute *attr);
 extern int cpu_add_dev_attr_group(struct attribute_group *attrs);
 extern void cpu_remove_dev_attr_group(struct attribute_group *attrs);
 
+extern int sched_create_sysfs_power_savings_entries(struct device *dev);
+
 #ifdef CONFIG_HOTPLUG_CPU
 extern void unregister_cpu(struct cpu *cpu);
 extern ssize_t arch_cpu_probe(const char *, size_t);
@@ -223,7 +225,9 @@ void idle_notifier_unregister(struct notifier_block *n);
 void idle_notifier_call_chain(unsigned long val);
 
 extern bool check_cpuboost(int cpu);
+#ifdef CONFIG_STATE_NOTIFIER
 extern bool wakeup_boost;
+#endif
 
 #endif /* _LINUX_CPU_H_ */
 
