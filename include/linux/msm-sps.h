5349f363800cb4e17df9cc782ac43ba700bc4651
Minia89 <n.miniati@gmail.com>
2015-05-14 14:21:56 +0200

    Revert "fix error"
    This reverts commit 9f9c40c1a456e6924195b15a633cb572a1b90ec8.

--- include/linux/msm-sps.h
+++ include/linux/msm-sps.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
 =======
 /* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
@@ -112,10 +113,13 @@
 #define SPS_BAM_NO_LOCAL_CLK_GATING (1UL << 5)
 /* Don't enable writeback cancel*/
 #define SPS_BAM_CANCEL_WB           (1UL << 6)
+<<<<<<< HEAD
 /* Verify Low Memory status */
 #define SPS_BAM_ATMC_MEM            (1UL << 8)
+=======
 /* Confirm resource status before access BAM*/
 #define SPS_BAM_RES_CONFIRM         (1UL << 7)
+>>>>>>> 1aabcc4... msm: sps: Squashed commit of upstream changes
 
 /* BAM device management flags */
 
@@ -183,8 +187,14 @@ enum sps_option {
 	SPS_O_IRQ_MTI   = 0x00020000,
 	/* NWD bit written with EOT for BAM2BAM producer pipe */
 	SPS_O_WRITE_NWD   = 0x00040000,
+<<<<<<< HEAD
+       /* EOT set after pipe SW offset advanced */
+	SPS_O_LATE_EOT   = 0x00080000,
+
+=======
 	/* EOT set after pipe SW offset advanced */
 	SPS_O_LATE_EOT   = 0x00080000,
+>>>>>>> 1aabcc4... msm: sps: Squashed commit of upstream changes
 	/* Options to enable software features */
 	/* Do not disable a pipe during disconnection */
 	SPS_O_NO_DISABLE      = 0x00800000,
@@ -301,8 +311,11 @@ enum sps_callback_case {
 	SPS_CALLBACK_BAM_ERROR_IRQ = 1,     /* BAM ERROR IRQ */
 	SPS_CALLBACK_BAM_HRESP_ERR_IRQ,	    /* Erroneous HResponse */
 	SPS_CALLBACK_BAM_TIMER_IRQ,	    /* Inactivity timer */
+<<<<<<< HEAD
+=======
 	SPS_CALLBACK_BAM_RES_REQ,	    /* Request resource */
 	SPS_CALLBACK_BAM_RES_REL,	    /* Release resource */
+>>>>>>> 1aabcc4... msm: sps: Squashed commit of upstream changes
 };
 
 /*
@@ -1333,6 +1346,9 @@ int sps_get_bam_debug_info(unsigned long dev, u32 option, u32 para,
  *
  */
 int sps_ctrl_bam_dma_clk(bool clk_on);
+<<<<<<< HEAD
+=======
+
 /*
  * sps_pipe_reset - reset a pipe of a BAM.
  * @dev:	BAM device handle
@@ -1353,6 +1369,7 @@ int sps_pipe_reset(unsigned long dev, u32 pipe);
  * Return: 0 on success, negative value on error
  */
 int sps_bam_process_irq(unsigned long dev);
+>>>>>>> 1aabcc4... msm: sps: Squashed commit of upstream changes
 #else
 static inline int sps_register_bam_device(const struct sps_bam_props
 			*bam_props, unsigned long *dev_handle)
@@ -1520,6 +1537,8 @@ static inline int sps_ctrl_bam_dma_clk(bool clk_on)
 {
 	return -EPERM;
 }
+<<<<<<< HEAD
+=======
 
 static inline int sps_pipe_reset(unsigned long dev, u32 pipe)
 {
@@ -1530,6 +1549,7 @@ static inline int sps_bam_process_irq(unsigned long dev)
 {
 	return -EPERM;
 }
+>>>>>>> 1aabcc4... msm: sps: Squashed commit of upstream changes
 #endif
 
 #endif /* _SPS_H_ */
