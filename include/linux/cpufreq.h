1e70adb9d6e0860d064319138f5a4517bd07f9bd
Minia89 <n.miniati@gmail.com>
2015-07-20 01:20:41 +0200

    Revert "Revert "Revert "cpufreq: despair governor"""
    This reverts commit 2e2fab961e154ad0a3ba69ccd2c3c8a450e4b5ca.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -448,9 +448,6 @@ extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
-extern struct cpufreq_governor cpufreq_gov_despair;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_despair)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
